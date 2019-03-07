3d44aea27546ffb0d5d08987ff5975a7f846f415
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-03-07 14:54:31 +0100

    Finnish html/css/js

--- client/script.js
+++ client/script.js
@@ -1,14 +1,47 @@
 'use strict';
 
-const button = document.querySelector('.button');
+const button = document.querySelector('.container_button');
 button.addEventListener('click', buttonClickHandler);
 
-async function buttonClickHandler() {
-  try {
-    const result = await fetch(`http://127.0.0.1:8888/test.php`);
-    const jsonData = await result.json();
-    console.log(jsonData);
-  } catch (error) {
-    console.log(error);
-  }
-}
+function buttonClickHandler() {
+  fetch(`http://127.0.0.1:8888/test.php`)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('The response was not OK.');
+    }
+  })
+  .then(result => {
+    writeDataToDOM(result);
+    console.log(result);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+};
+
+function writeDataToDOM(data) {
+ const container = document.querySelector('.js-container_content');
+
+ Object.keys(data).forEach(day => {
+  let html;
+
+  data[day].forEach(event => {
+    html = `
+      <div class="content_activity">
+        <h2>${day}</h2>
+        <div class="activity">
+          <p class="activity_type">${event.activity}</p>
+          <p class="activity_time">od ${event.time_from}</p>
+          <p class="activity_time">do ${event.time_to}</p>
+        </div>
+        <p>${event.activity_detail}</p>
+        <p>${event.activity_name}</p>
+      </div>
+    `;
+    container.insertAdjacentHTML('beforeend', html);
+  })
+ });
+
+};
